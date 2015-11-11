6abdf595519db6b078a1db708668d18a373dde6f
Nylon <diego.gomes87@gmail.com>
2015-11-10 23:01:27 -0200

    Toda ver que for mexer com array tera que usar essas três funções: func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int
    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent
    component: Int) -> Int
    
    func pickerView(pickerView: UIPickerView, didSelectRow row: Int,
    inComponent component: Int)

--- FoodInWorld/ViewController.swift
+++ FoodInWorld/ViewController.swift
@@ -15,8 +15,8 @@ class ViewController: UIViewController,UIPickerViewDelegate {
     @IBOutlet var label2: UILabel!
     @IBOutlet var menssage: UILabel!
 
-    
-    var data = [[]]
+    //atencao aqui nesse array,é aqui que teremos dois pickerview lado a lado,diferente do que eu pensei da primeira vez que teria dois pickerview lado a lado
+    var data = [["USA","Italy","Grermany","Brazil","Japon","Chinese"],["Beijing","Hong Kong","Tailand","Ireland","London","France"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +28,22 @@ class ViewController: UIViewController,UIPickerViewDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int{
+        return  data.count
+    }
+    
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return data[component].count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return data[component][row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        let item1 = data[0][pickerView.selectedRowInComponent(0)]
+        let item2 = data[1][pickerView.selectedRowInComponent(1)]
+    }
 }
 
